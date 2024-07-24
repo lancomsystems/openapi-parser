df783175c239a5d0e83b70d6d61c5109f02fe778
Dennis Rieks <dennis.rieks@rohde-schwarz.com>
2024-07-24 16:17:45 +0200

    New: added float and binary to SchemaFormat

--- src/main/kotlin/de/lancom/openapi/common/types/SchemaFormat.kt
+++ src/main/kotlin/de/lancom/openapi/common/types/SchemaFormat.kt
@@ -17,8 +17,10 @@ data class SchemaFormat(val format: String) : JsonWrapper {
         val UUID = SchemaFormat("uuid")
         val INT32 = SchemaFormat("int32")
         val INT64 = SchemaFormat("int64")
+        val FLOAT = SchemaFormat("float")
         val DATE = SchemaFormat("date")
         val DATE_TIME = SchemaFormat("date-time")
         val EMAIL = SchemaFormat("email")
+        val BINARY = SchemaFormat("binary")
     }
 }
